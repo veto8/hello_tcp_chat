f24ee901a99dd057b7f0cff50c8cab53824feb81
veto <veto@myridia.com>
2025-09-25 10:05:54 +0700

    update

--- clients/browser/client.js
+++ clients/browser/client.js
@@ -1,64 +1,39 @@
 var socket = new WebSocket("ws://127.0.0.1:8765");
+var msg_box = document.querySelector("#msg_box");
+msg_box.value = "";
+var name = "";
+
 socket.onopen = function (e) {
-  console.log("...open");
+  log("...open");
 };
+
 socket.onmessage = function (event) {
-  console.log("...message from server");
-  console.log(event.data);
+  //console.log("...message from server");
+  log(event.data);
 };
+
 socket.onclose = function (event, code) {
-  console.log("...close");
+  log("...close");
 };
 
 socket.onerror = function (event, code) {
-  console.log("...error");
+  log("...error");
 };
 
 let send_button = document.querySelector("#send_button");
 
 send_button.addEventListener("click", (event) => {
   let msg = document.querySelector("#message").value;
+  document.querySelector("#message").value = "";
+  if (name === "") {
+    name = "[" + msg + "] ";
+  }
+
   msg += "\n";
   socket.send(msg);
+  log(name + msg);
 });
 
-/*
-
-
-window.onload = async function () {
-  socket.addEventListener("open", () => {
-    console.log("...open socket");
-  });
-
-  socket.addEventListener("error", (event) => {
-    console.log("ERROR");
-  });
-
-  socket.addEventListener("close", (event) => {
-    console.log("The connection has been closed successfully.");
-  });
-
-  socket.addEventListener("message", (event) => {
-    console.log("...get message");
-    var decoder = new TextDecoder("utf-8");
-    const t = decoder.decode(event.data);
-
-    console.log(t);
-  });
-
-  let send_button = document.querySelector("#send_button");
-
-  send_button.addEventListener("click", (event) => {
-    let msg = document.querySelector("#message").value;
-    msg += "\n";
-    console.log(msg);
-    //socket.send(msg);
-    socket.send(JSON.stringify({ type: "heartbeat" }));
-  });
-};
-
-async function convert_text(blob) {
-  const text = await new Response(blob).text();
-  return text;
+function log(msg = "") {
+  msg_box.value += msg + "\n";
 }
-*/
